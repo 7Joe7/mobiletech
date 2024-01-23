3c4119c029cd329799b558cefc232daf3285a190
Josef Erneker <josef.erneker@gmail.com>
2024-01-23 23:14:03 +0100

    Add weather fields and migration

--- app/src/main/java/com/erneker/weather/DetailActivity.kt
+++ app/src/main/java/com/erneker/weather/DetailActivity.kt
@@ -29,11 +29,14 @@ class DetailActivity : AppCompatActivity() {
         // Get the passed location name
         val locationName = intent.getStringExtra("LOCATION_NAME").toString()
         viewModel.locationValue.observe(this) { location ->
-            binding.textLocation.text = location.name + " (" + location.country + ")"
-            binding.textLatLon.text = "Latitude: " +
-                    viewModel.locationValue.value?.lat.toString() +
-                    ", longitude: " +
-                    viewModel.locationValue.value?.lon.toString()
+            binding.textLocation.text = "${location.name} (${location.country})"
+            binding.textLatLon.text = "Latitude: ${viewModel.locationValue.value?.lat.toString()},  longitude: ${viewModel.locationValue.value?.lon.toString()}"
+            binding.textTimestamp.text = "Date: ${location.timestamp}"
+            binding.textTemp.text = "Temperature: ${location.temp.toString()}"
+            binding.textHumidity.text = "Humidity: ${location.humidity.toString()}"
+            binding.textPressure.text = "Pressure: ${location.pressure.toString()}"
+            binding.textWindSpeed.text = "Wind Speed: ${location.windSpeed.toString()}"
+            binding.textVisibility.text = "Visibility: ${location.visibility.toString()}"
         }
         viewModel.getLocation(locationName)
     }
--- app/src/main/java/com/erneker/weather/database/RoomDatabase.kt
+++ app/src/main/java/com/erneker/weather/database/RoomDatabase.kt
@@ -8,6 +8,8 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -20,19 +22,33 @@ interface LocationDao {
     fun insert( location: LocationDTO)
 }
 
-@Database(entities = [LocationDTO::class], version = 1)
+@Database(entities = [LocationDTO::class], version = 2)
 abstract class MyRoomDatabase: RoomDatabase() {
     abstract val locationDao: LocationDao
 }
 
 private lateinit var INSTANCE: MyRoomDatabase
 
+val MIGRATION_1_2: Migration = object : Migration(1, 2) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        // Since we are adding columns, we can use the SQL ALTER TABLE command
+        database.execSQL("ALTER TABLE location ADD COLUMN temp REAL NOT NULL DEFAULT 0")
+        database.execSQL("ALTER TABLE location ADD COLUMN pressure INTEGER NOT NULL DEFAULT 0")
+        database.execSQL("ALTER TABLE location ADD COLUMN humidity INTEGER NOT NULL DEFAULT 0")
+        database.execSQL("ALTER TABLE location ADD COLUMN windSpeed REAL NOT NULL DEFAULT 0")
+        database.execSQL("ALTER TABLE location ADD COLUMN visibility INTEGER NOT NULL DEFAULT 0")
+        database.execSQL("ALTER TABLE location ADD COLUMN timestamp TEXT NOT NULL DEFAULT ''")
+    }
+}
+
 fun getDatabase(context: Context): MyRoomDatabase {
     synchronized(MyRoomDatabase::class.java) {
         if (!::INSTANCE.isInitialized) {
             INSTANCE = Room.databaseBuilder(context.applicationContext,
                 MyRoomDatabase::class.java,
-                "my_room_database").build()
+                "my_room_database")
+                .addMigrations(MIGRATION_1_2) // Add this line to add the migration
+                .build()
         }
     }
     return INSTANCE
